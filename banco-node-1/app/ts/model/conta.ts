d955b3b73c4df08a0f0f3e65dfd258432652669d
rebek <rebekamrr01@outlook.com>
2021-07-31 18:11:57 -0300

    install commit

---
+++ banco-node-1/app/ts/model/conta.ts
@@ -0,0 +1,33 @@
+class Conta {
+    private _numero;
+    private _saldo;
+
+    constructor(numero, saldo=0) {
+        this._numero = numero;
+        this._saldo = saldo;
+    }
+
+    get numero() {
+        return this._numero;
+    }
+
+    set numero(numero) {
+        this._numero = numero;
+    }
+
+    get saldo() {
+        return this._saldo;
+    }
+
+    debitar(valor) {
+        //apenas debita se houver saldo
+        if (valor < this._saldo) {
+            this._saldo -= valor;
+        }
+    }
+
+    creditar(valor) {
+        this._saldo += valor;
+    }
+
+}
